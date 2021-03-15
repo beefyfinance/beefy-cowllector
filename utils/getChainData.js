933eda90aaca6fb2b81a08fb13f9a13cff7fd456
MirthFutures <69535002+MirthFutures@users.noreply.github.com>
2021-03-15 10:14:27 -0400

    Add Avalanche

--- utils/getChainData.js
+++ utils/getChainData.js
@@ -4,6 +4,8 @@ const getChainBlockTime = chainId => {
       return 3;
     case 128:
       return 3;
+    case 43114:
+      return 3;
     default:
       throw new Error(`Chain ID ${chainId} is not valid.`);
   }
@@ -15,6 +17,8 @@ const getChainRpc = chainId => {
       return process.env.BSC_RPC;
     case 128:
       return process.env.HECO_RPC;
+    case 43114:
+      return process.env.AVAX_RPC;
     default:
       throw new Error(`Chain ID ${chainId} is not valid.`);
   }
