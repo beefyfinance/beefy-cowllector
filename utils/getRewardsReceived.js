308fda3bb96a3beb4c9771b2b24de1d8b6b612f4
sirbeefalot <sirbeefalot@pm.me>
2021-04-06 20:20:00 -0300

    adjust to lower rate limits

--- utils/getRewardsReceived.js
+++ utils/getRewardsReceived.js
@@ -4,8 +4,8 @@ const { sleep } = require('./harvestHelpers');
 
 const { getTopicFromSignature, getTopicFromAddress, getValueFromData } = require('./topicHelpers');
 
-const RPC_QUERY_LIMIT = 5000;
-const RPC_QUERY_INTERVAL = 200;
+const RPC_QUERY_LIMIT = 2000;
+const RPC_QUERY_INTERVAL = 300;
 const FIRST_REWARD_BLOCK = 1457038;
 
 const REWARD_POOL = '0x453D4Ba9a2D594314DF88564248497F7D74d6b2C';
@@ -14,54 +14,60 @@ const WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const web3 = new Web3(process.env.BSC_RPC);
 
 const getRewardsReceived = async () => {
-  let result = new BigNumber(0);
+  try {
+    let result = new BigNumber(0);
 
-  const lastBlock = await web3.eth.getBlockNumber();
-  const details = await getLastRewardAddedDetails(lastBlock);
+    const lastBlock = await web3.eth.getBlockNumber();
+    const details = await getLastRewardAddedDetails(lastBlock);
 
-  const transferTopic = getTopicFromSignature('Transfer(address,address,uint256)');
-  const toTopic = getTopicFromAddress(REWARD_POOL);
+    const transferTopic = getTopicFromSignature('Transfer(address,address,uint256)');
+    const toTopic = getTopicFromAddress(REWARD_POOL);
 
-  let fromBlock = details.blockNumber;
+    let fromBlock = details.blockNumber;
 
-  while (fromBlock < lastBlock) {
-    let toBlock = fromBlock + RPC_QUERY_LIMIT;
-    if (toBlock > lastBlock) { 
-      toBlock = lastBlock;
-    }
-
-    const logs = await web3.eth.getPastLogs({
-      fromBlock: fromBlock,
-      toBlock: toBlock - 1,
-      address: WBNB,
-      topics: [transferTopic, null, toTopic],
-    });
-
-    for (let i = 0; i < logs.length; i++) {
-      if (
-        logs[i].blockNumber === details.blockNumber &&
-        logs[i].transactionIndex < details.transactionIndex
-      ) {
-        continue;
+    while (fromBlock < lastBlock) {
+      let toBlock = fromBlock + RPC_QUERY_LIMIT;
+      if (toBlock > lastBlock) {
+        toBlock = lastBlock;
       }
-      const value = getValueFromData(logs[i].data);
-      result = result.plus(value);
+
+      const logs = await web3.eth.getPastLogs({
+        fromBlock: fromBlock,
+        toBlock: toBlock - 1,
+        address: WBNB,
+        topics: [transferTopic, null, toTopic],
+      });
+
+      for (let i = 0; i < logs.length; i++) {
+        if (
+          logs[i].blockNumber === details.blockNumber &&
+          logs[i].transactionIndex < details.transactionIndex
+        ) {
+          continue;
+        }
+        const value = getValueFromData(logs[i].data);
+        result = result.plus(value);
+      }
+
+      await sleep(RPC_QUERY_INTERVAL);
+
+      fromBlock = toBlock;
     }
-   
-    await sleep(RPC_QUERY_INTERVAL);
 
-    fromBlock = toBlock;
+    console.log('Result', result.toString());
+
+    return result;
+  } catch (e) {
+    console.log(e);
   }
-
-  return result;
 };
 
-const getLastRewardAddedDetails = async (lastBlock) => {
+const getLastRewardAddedDetails = async lastBlock => {
   const topic = getTopicFromSignature('RewardAdded(uint256)');
-  
+
   let fromBlock = lastBlock - RPC_QUERY_LIMIT;
   let toBlock = lastBlock;
-  
+
   let details = { blockNumber: 0, transactionIndex: 0 };
 
   while (fromBlock > FIRST_REWARD_BLOCK) {
