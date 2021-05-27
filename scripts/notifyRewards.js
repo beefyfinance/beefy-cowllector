81429b120442a4854a3a9d6b7c3e08ad4f184688
sirbeefalot <sirbeefalot@pm.me>
2021-05-27 15:43:58 -0300

    Add error handling to notifyRewards

--- scripts/notifyRewards.js
+++ scripts/notifyRewards.js
@@ -7,23 +7,27 @@ const getRewardsReceived = require('../utils/getRewardsReceived');
 const chains = require('../data/chains');
 
 const notifyRewards = async () => {
-  for (chain of chains) {
+  for (chain of Object.values(chains)) {
     if (!chain.rewardPool) continue;
 
-    const provider = new ethers.providers.JsonRpcProvider(chain.rpc);
-    const harvester = new ethers.Wallet(process.env.REWARDER_PRIVATE_KEY, provider);
-    const rewardsContract = new ethers.Contract(chain.rewardPool, RewardPool, harvester);
-    const wnativeContract = new ethers.Contract(chain.wnative, WrappedNative, harvester);
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(chain.rpc);
+      const harvester = new ethers.Wallet(process.env.REWARDER_PRIVATE_KEY, provider);
+      const rewardsContract = new ethers.Contract(chain.rewardPool, RewardPool, harvester);
+      const wnativeContract = new ethers.Contract(chain.wnative, WrappedNative, harvester);
 
-    const rewardsReceived = await getRewardsReceived(chain);
-    let balance = await wnativeContract.balanceOf(chain.rewardPool);
-    balance = new BigNumber(balance.toString());
+      const rewardsReceived = await getRewardsReceived(chain);
+      let balance = await wnativeContract.balanceOf(chain.rewardPool);
+      balance = new BigNumber(balance.toString());
 
-    if (rewardsReceived.lte(balance)) {
-      await rewardsContract.notifyRewardAmount(rewardsReceived.toFixed());
-      console.log(`${rewardsReceived.div('1e18').toFixed()} in rewards notified on ${chain.id}.`);
-    } else {
-      console.log('Attempting to notify more than balance', rewardsReceived.toFixed());
+      if (rewardsReceived.lte(balance)) {
+        await rewardsContract.notifyRewardAmount(rewardsReceived.toFixed());
+        console.log(`${rewardsReceived.div('1e18').toFixed()} in rewards notified on ${chain.id}.`);
+      } else {
+        console.log('Attempting to notify more than balance', rewardsReceived.toFixed());
+      }
+    } catch (e) {
+      console.log(`Something went wrong with chain ${chain.chainId}: ${e}`);
     }
   }
 };
