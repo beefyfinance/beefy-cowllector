bbde6f12b07c4caec1b6d9bc9831001d532a19a3
Broken Robot <loewen.nathan@gmail.com>
2021-06-11 23:55:00 -0500

    Add try/catch to addVaultFromCli.js

--- scripts/addVaultFromCli.js
+++ scripts/addVaultFromCli.js
@@ -13,23 +13,28 @@ if (!args.length) {
 }
 
 const main = async () => {
-  const { newVaults, newStrats } = await addVault({
-    vault: args[0],
-    chainId: Number(args[1]) || 56,
-    interval: Number(args[2]) || 6,
-    vaults,
-    strats,
-  });
+  try {
+    const { newVaults, newStrats } = await addVault({
+      vault: args[0],
+      chainId: Number(args[1]) || 56,
+      interval: Number(args[2]) || 6,
+      vaults,
+      strats,
+    });
 
-  fs.writeFileSync(
-    path.resolve(__dirname, '..', 'data', 'defistation.json'),
-    JSON.stringify(newVaults, null, 2)
-  );
+    fs.writeFileSync(
+      path.resolve(__dirname, '..', 'data', 'defistation.json'),
+      JSON.stringify(newVaults, null, 2)
+    );
 
-  fs.writeFileSync(
-    path.resolve(__dirname, '..', 'data', 'strats.json'),
-    JSON.stringify(newStrats, null, 2)
-  );
+    fs.writeFileSync(
+      path.resolve(__dirname, '..', 'data', 'strats.json'),
+      JSON.stringify(newStrats, null, 2)
+    );
+  }
+  catch (e) {
+    console.error(e);
+  }
 };
 
 main();
