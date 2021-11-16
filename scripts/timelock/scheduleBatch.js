5c8ae5bd72dbafd3178ae5a09d1f909f34b7632d
sirb <sirbeefalot@pm.me>
2021-11-16 00:29:52 -0300

    Create script to batch schedule transactions

---
+++ scripts/timelock/scheduleBatch.js
@@ -0,0 +1,46 @@
+const ethers = require('ethers');
+
+const TimelockAbi = require('../../abis/TimelockController.json');
+
+const scheduleBatch = async ({
+  timelockAddr,
+  targets,
+  values,
+  data,
+  predecessor,
+  salt,
+  signer,
+}) => {
+  const timelock = new ethers.Contract(timelockAddr, TimelockAbi, signer);
+
+  try {
+    const operationHash = await timelock.hashOperationBatch(
+      targets,
+      values,
+      data,
+      predecessor,
+      salt
+    );
+
+    const isOperation = await timelock.isOperation(operationHash);
+    if (isOperation) {
+      console.log(`Tx is scheduled already, skipping.`);
+      return;
+    }
+
+    const minDelay = await timelock.getMinDelay();
+
+    let tx = await timelock.scheduleBatch(targets, values, data, predecessor, salt, minDelay, {
+      gasLimit: 3000000,
+    });
+    tx = await tx.wait();
+
+    tx.status === 1
+      ? console.log(`Scheduled with tx: ${tx.transactionHash}`)
+      : console.log(`Failed with tx: ${tx.transactionHash}`);
+  } catch (e) {
+    console.log(`Something went wrong: ${e}`);
+  }
+};
+
+module.exports = scheduleBatch;
