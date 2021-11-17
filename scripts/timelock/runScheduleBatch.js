2c595ab551207e8bbc2ef187482748647b2012f4
sirb <sirbeefalot@pm.me>
2021-11-17 19:53:15 -0300

    prepare tx

--- scripts/timelock/runScheduleBatch.js
+++ scripts/timelock/runScheduleBatch.js
@@ -5,17 +5,23 @@ const chains = require('../../data/chains');
 const scheduleBatch = require('./scheduleBatch');
 const chainIdFromName = require('../../utils/chainIdFromName');
 
-const chainName = 'polygon';
-const targets = [];
+const chainName = 'fantom';
+const targets = [
+  '0xB2aC5329D7D78Cb208964C566c35f0eF8C6572f6',
+  '0x1f71777c9db9fe5b19927dDF33Efa22191E0B354',
+  '0x12F1A8C91Ffdb0aF421b703A75b8E9f57ADDF0b9',
+  '0xa623c28f64A429baE0426Aa8AEbE64d307A38cC0',
+  '0xd88c449c1F68480316f3e3A48bD37186e58217A8',
+];
 
 const config = {
-  timelockAddress: addressBook[chainName].platforms.beefyfinance.strategyOwner,
+  timelockAddress: '0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F',
   chainId: chainIdFromName(chainName),
   pk: process.env.UPGRADER_PK,
   values: Array.from({ length: targets.length }, () => 0),
   data: Array.from(
     { length: targets.length },
-    () => '0xf2fde38b000000000000000000000000847298ac8c28a9d66859e750456b92c2a67b876d'
+    () => '0xa68833e5000000000000000000000000502c107ae28d300fdaede1cbd7ee8096c1ab4a3c'
   ),
   predecessor: ethers.constants.HashZero,
   salt: ethers.constants.HashZero,
