ea5be24d02fb63e70a117a647e8f33337990a445
sirb <sirbeefalot@pm.me>
2021-11-25 02:03:20 -0300

    Add 'lastHarvest' to accelerate harvest script execution

--- scripts/harvest.js
+++ scripts/harvest.js
@@ -18,12 +18,24 @@ const harvest = async () => {
       const harvester = new ethers.Wallet(process.env.HARVESTER_PK, provider);
       const keeper = new ethers.Wallet(process.env.KEEPER_PK, provider);
 
+      const stratContract = new ethers.Contract(strat.address, IStrategy, harvester);
+
       let shouldHarvest = true;
 
       if (shouldHarvest) shouldHarvest = !strat.harvestPaused;
-      if (shouldHarvest) shouldHarvest = await hasStakers(strat, harvester);
+      if (shouldHarvest) shouldHarvest = await hasStakers(stratContract);
+
+      let lastHarvest = 0;
+      try {
+        lastHarvest = await stratContract.lastHarvest();
+      } catch (err) {}
+
       if (shouldHarvest) {
-        if (strat.noHarvestEvent) {
+        if (lastHarvest !== 0) {
+          let now = Math.floor(new Date().getTime() / 1000);
+          let secondsSinceHarvest = now - lastHarvest;
+          shouldHarvest = secondsSinceHarvest >= strat.interval * 3600;
+        } else if (strat.noHarvestEvent) {
           shouldHarvest = await isNewPeriodNaive(strat.interval);
         } else {
           shouldHarvest = await isNewHarvestPeriod(strat, harvester);
@@ -44,7 +56,7 @@ const harvest = async () => {
           //   : console.log(`${strat.name} harvest failed with tx: ${tx.transactionHash}`);
           // await stratContract.connect(keeper).pause({ gasLimit: 3500000 });
         } else {
-          tx = await stratContract.harvest({ gasLimit: 4000000 });
+          tx = await stratContract.harvest({ gasLimit: 5000000 });
           // FIX: Polygon is taking too long to confirm. Harvesting without waiting for mining for now.
           // tx = await tx.wait();
           // tx.status === 1
@@ -60,7 +72,7 @@ const harvest = async () => {
       console.log(`Couldn't harvest strat ${strat.name}: ${e}`);
     }
 
-    await sleep(2000);
+    await sleep(1000);
   }
 };
 
