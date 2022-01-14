abdb45d923fb45f8a7783518da0e84cbbda55abf
0xww <mynameiswalterwhite@pm.me>
2022-01-13 22:54:08 -0300

    fix: export harvest func

--- scripts/harvest.js
+++ scripts/harvest.js
@@ -3,14 +3,16 @@ const _ = require('lodash');
 
 const strats = require('../data/strats.json');
 const groups = _.groupBy(strats, 'chainId');
-let children = {};
-// let chainId = 56;
-for (const chainId in groups) {
-  if (Object.hasOwnProperty.call(groups, chainId)) {
-    let child = cp.fork('./scripts/harvestChild.js', [chainId]);
-    child.on('message', msg => {
-      console.log(msg);
-    });
-    children[chainId] = child;
+
+const main = async () => {
+  for (const chainId in groups) {
+    if (Object.hasOwnProperty.call(groups, chainId)) {
+      let child = cp.fork('./scripts/harvestChild.js', [chainId]);
+      child.on('message', msg => {
+        console.log(msg);
+      });
+    }
   }
-}
+};
+
+module.exports = main;
