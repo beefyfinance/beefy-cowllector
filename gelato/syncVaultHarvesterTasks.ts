0a70819f542c4191b06e38ad21dfa08d9a67df06
Moonster <moonster@beefy.com>
2022-02-25 00:27:00 -0500

    done, to test

--- gelato/syncVaultHarvesterTasks.ts
+++ gelato/syncVaultHarvesterTasks.ts
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
+import { ethers, Wallet } from 'ethers';
 import fs from 'fs';
+import { GelatoClient } from './gelatoClient';
 import { VaultConfig } from './interfaces/VaultConfig';
 
 export const syncVaultHarvesterTasks = async (chainName: string) => {
@@ -12,13 +14,18 @@ export const syncVaultHarvesterTasks = async (chainName: string) => {
     const chainVaults = data.filter(vault => vault.chain === chainName)
     const activeVaultMap = getActiveVaults(chainVaults);
 
-    console.log(JSON.stringify(activeVaultMap));
+    const provider = new ethers.providers.JsonRpcProvider("rpc");
+    const gelatoAdminWallet: Wallet = new Wallet(process.env.GELATO_ADMIN_PK!, provider);
+    const harvesterAddress = "0x5e7F411EE92838275c96438B6A1A93acCC16364C";
+    const opsAddress = "0x6EDe1597c05A0ca77031cBA43Ab887ccf24cd7e8"
+    const gelatoClient = new GelatoClient(gelatoAdminWallet, harvesterAddress, opsAddress);
+    await gelatoClient.createTasks(activeVaultMap);
   } else {
     console.log('Fetching vaults failed');
   }
 };
 
-const getActiveVaults = (vaultList: VaultConfig[]) => {
+const getActiveVaults = (vaultList: VaultConfig[]): Record<string, string> => {
   const vaults: Record<string, string> = {};
   for (const vault of vaultList) {
     if (vault.id.endsWith('eol')) {
@@ -27,5 +34,5 @@ const getActiveVaults = (vaultList: VaultConfig[]) => {
       vaults[vault.earnedToken] = vault.earnedTokenAddress;
     }
   }
-  console.log(JSON.stringify(vaults));
+  return vaults;
 };
