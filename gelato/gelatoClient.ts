b8a572f1f4030baa50b450b922dafdb3a5099cb8
Moonster <moonster@beefy.com>
2022-02-25 09:59:48 -0500

    computeTaskId

--- gelato/gelatoClient.ts
+++ gelato/gelatoClient.ts
@@ -1,18 +1,60 @@
-import { ethers, Wallet } from 'ethers';
+import { Contract, ethers, Wallet } from 'ethers';
 import OPS_ABI from './abis/Ops.json';
 
 export class GelatoClient {
-  readonly gelatoAdmin: Wallet;
-  readonly harvesterAddress: string;
-  readonly opsAddress: string;
+  private static readonly _feeTokenWhenNotPrepaidTask = "0x0000000000000000000000000000000000000000"; // When task is not prepaid, the contract just uses 0 address, since it uses any funds in the treasury.
+
+  private readonly _gelatoAdmin: Wallet;
+  private readonly _harvesterAddress: string;
+  private readonly _opsContract: Contract;
 
   constructor(gelatoAdmin_: Wallet, harvesterAddress_: string, opsAddress_: string) {
-    this.gelatoAdmin = gelatoAdmin_;
-    this.harvesterAddress = harvesterAddress_;
-    this.opsAddress = opsAddress_;
+    this._gelatoAdmin = gelatoAdmin_;
+    this._harvesterAddress = harvesterAddress_;
+    this._opsContract = new ethers.Contract(opsAddress_, OPS_ABI, this._gelatoAdmin);
   }
 
-  async createTasks(vaultMap: Record<string, string>) {
+  public async computeTaskId(vault_: string) {
+    const performSelector = await this._opsContract.getSelector("performUpkeep(address,uint256,uint256,uint256,uint256,bool)");
+    const checkerSelector = (await this._opsContract.getSelector("checker(address)"));
+    const replaced0x = `000000000000000000000000${vault_.toLowerCase().slice(2)}`
+    const resolverData = `${checkerSelector}${replaced0x}`
+    const useTaskTreasuryFunds = true;
+  
+    console.log(`Getting resolver hash for ${vault_}`);
+  
+    console.log("getResolverHash data:");
+    console.log(`resolver: ${this._harvesterAddress}`);
+    console.log(`resolverData: ${resolverData}`);
+  
+    const resolverHash = await this._opsContract.getResolverHash(
+      this._harvesterAddress,
+      resolverData
+    )
+  
+    console.log(`Getting taskId for vault: ${vault_}`);
+  
+    console.log("getTaskId data:");
+    console.log(`taskCreator: ${this._gelatoAdmin.address}`)
+    console.log(`execAddress: ${this._harvesterAddress}`);
+    console.log(`selector: ${performSelector}`);
+    console.log(`useTaskTreasuryFunds: ${useTaskTreasuryFunds}`);
+    console.log(`feeToken: ${GelatoClient._feeTokenWhenNotPrepaidTask}`);
+    console.log(`resolverHash: ${resolverHash}`);
+  
+    const id = await this._opsContract.getTaskId(
+      this._gelatoAdmin.address,
+      this._harvesterAddress,
+      performSelector,
+      useTaskTreasuryFunds,
+      GelatoClient._feeTokenWhenNotPrepaidTask,
+      resolverHash
+    );
+  
+    console.log(`Task id: ${id}`);
+  }
+
+  public async createTasks(vaultMap: Record<string, string>) {
     for (const key in vaultMap) {
       const vault = vaultMap[key];
       console.log(`Creating task for ${key}`);
@@ -27,25 +69,24 @@ export class GelatoClient {
   }
 
   private async createTask(vault: string) {
-    const ops = new ethers.Contract(this.opsAddress, OPS_ABI, this.gelatoAdmin);
 
-    const performSelector = await ops.getSelector(
+    const performSelector = await this._opsContract.getSelector(
       'performUpkeep(address,uint256,uint256,uint256,uint256,bool)'
     );
-    const checkerSelector = await ops.getSelector('checker(address)');
+    const checkerSelector = await this._opsContract.getSelector('checker(address)');
     const replaced0x = `000000000000000000000000${vault.toLowerCase().slice(2)}`;
     const resolverData = `${checkerSelector}${replaced0x}`;
 
     console.log('Create task data:');
-    console.log(`execAddress: ${this.harvesterAddress}`);
+    console.log(`execAddress: ${this._harvesterAddress}`);
     console.log(`execSelector: ${performSelector}`);
-    console.log(`resolverAddress: ${this.harvesterAddress}`);
+    console.log(`resolverAddress: ${this._harvesterAddress}`);
     console.log(`resolverData: ${resolverData}`);
 
-    const txn = await ops.createTask(
-      this.harvesterAddress,
+    const txn = await this._opsContract.createTask(
+      this._harvesterAddress,
       performSelector,
-      this.harvesterAddress,
+      this._harvesterAddress,
       resolverData
     );
 
