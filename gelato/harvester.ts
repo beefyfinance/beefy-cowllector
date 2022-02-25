24018eb8753e71062dbcc394b616285b16b5babd
Moonster <moonster@beefy.com>
2022-02-25 15:38:08 -0500

    harvester

--- gelato/harvester.ts
+++ gelato/harvester.ts
@@ -1,18 +1,26 @@
 import { Contract, ethers, Wallet } from 'ethers';
+import { BeefyAppClient } from './beefyAppClient';
 
 export class Harvester {
+  private readonly _cowllector: Wallet;
+  private readonly _beefyAppClient: BeefyAppClient;
+  private readonly _chainName: string;
+  private readonly _vaultDenylist: Set<string>;
+  private readonly _harvesterAddress: string;
 
-    private readonly _cowllector: Wallet;
-    private readonly _harvesterAddress: string;
-    private readonly _vaultsArrayJsEndpoint: string;
-    private readonly _vaultDenylist: Set<string>;
+  constructor(
+    cowllector_: Wallet,
+    chainName_: string,
+    harvesterAddress_: string,
+    opsAddress_: string,
+    vaultDenylist_: Set<string>
+  ) {
+    this._cowllector = cowllector_;
+    this._beefyAppClient = new BeefyAppClient();
+    this._chainName = chainName_;
+    this._vaultDenylist = vaultDenylist_;
+    this._harvesterAddress = harvesterAddress_;
+  }
 
-    constructor(cowllector_: Wallet, vaultsArrayJsEndpoint_: string, harvesterAddress_: string, opsAddress_: string, vaultDenylist_: Set<string>) {
-        this._cowllector = cowllector_;
-        this._vaultsArrayJsEndpoint = vaultsArrayJsEndpoint_;
-        this._harvesterAddress = harvesterAddress_;
-        this._vaultDenylist = vaultDenylist_;
-    }
-
-    
+  
 }
