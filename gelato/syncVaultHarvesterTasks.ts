b0624a991b3809a8ccc21b624782301b84d80877
Moonster <moonster@beefy.com>
2022-02-25 10:54:27 -0500

    add deny list

--- gelato/syncVaultHarvesterTasks.ts
+++ gelato/syncVaultHarvesterTasks.ts
@@ -1,40 +1,34 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-const fetch = require('node-fetch');
+
 import { ethers, Wallet } from 'ethers';
-import { GelatoClient } from './gelatoClient';
-import { VaultConfig } from './interfaces/VaultConfig';
+import { TaskSyncer } from './taskSyncer';
 
-export const syncVaultHarvesterTasks = async (chainName: string) => {
-  const response = await fetch(
-    "https://api.beefy.finance/vaults"
+export const syncFantomVaultHarvesterTasks = async () => {
+  const pk = process.env.GELATO_ADMIN_PK!;
+  const provider = new ethers.providers.JsonRpcProvider('https://rpc.ftm.tools');
+  const gelatoAdminWallet: Wallet = new Wallet(pk, provider);
+
+  const fantomHarvesterAddress = '0x5e7F411EE92838275c96438B6A1A93acCC16364C';
+  const fantomOpsAddress = '0x6EDe1597c05A0ca77031cBA43Ab887ccf24cd7e8';
+
+  const vaultDenyList = new Set([
+    'mooGeistMIM',
+    'mooGeistCRV',
+    'mooGeistFTM',
+    'mooGeistWBTC',
+    'mooGeistETH',
+    'mooGeistfUSDT',
+    'mooGeistUSDC',
+    'mooGeistDAI',
+    'mooGeistGEIST-WFTM',
+  ]);
+
+  const taskSyncer = new TaskSyncer(
+    gelatoAdminWallet,
+    'fantom',
+    fantomHarvesterAddress,
+    fantomOpsAddress,
+    vaultDenyList
   );
-  if (response.ok && response.body) {
-    const data: VaultConfig[] = await response.json();
-
-    const chainVaults = data.filter(vault => vault.chain === chainName)
-    const activeVaultMap = getActiveVaults(chainVaults);
-
-    const provider = new ethers.providers.JsonRpcProvider("https://rpc.ftm.tools");
-    const pk = process.env.GELATO_ADMIN_PK!
-    const gelatoAdminWallet: Wallet = new Wallet(pk, provider);
-    const harvesterAddress = "0x5e7F411EE92838275c96438B6A1A93acCC16364C";
-    const opsAddress = "0x6EDe1597c05A0ca77031cBA43Ab887ccf24cd7e8"
-    const gelatoClient = new GelatoClient(gelatoAdminWallet, harvesterAddress, opsAddress);
-    await gelatoClient.createTasks(activeVaultMap);
-  } else {
-    console.log('Fetching vaults failed');
-  }
-};
-
-const getActiveVaults = (vaultList: VaultConfig[]): Record<string, string> => {
-  const vaults: Record<string, string> = {};
-  for (const vault of vaultList) {
-    if (vault.id.endsWith('eol')) {
-      continue;
-    } else {
-      vaults[vault.earnedToken] = vault.earnedTokenAddress;
-    }
-  }
-  return vaults;
 };
