95d26b329aa2e255b1b7fc4d2e1cd2604ff992dc
AllTradesz <alltrades.beefy@gmail.com>
2022-08-30 17:56:44 +0200

    Operational Gelato-lifecycle sync (Link T-622)

--- scripts/syncStrats.ts
+++ scripts/syncStrats.ts
@@ -13,21 +13,12 @@ written to `data\stratsSync.json`.
 Run command: yarn ts-node --transpile-only scripts/syncStrats.ts
 ********/
 
-import FETCH, { type Response } from 'node-fetch'; //pull in of type Response
-//  needed due to clash with WebWorker's version
+import FETCH, { type Response } from 'node-fetch'; //pull in of type Response needed due to 
+                                                 //  clash with WebWorker's version
 import { ethers as ETHERS } from 'ethers';
 import FS from 'fs';
 import PATH from 'path';
-import { settledPromiseFilled, nodeJsError, type OptionalMutable } from '../utility/baseNode';
-import type {
-  IVault,
-  IStratExtendedProperties,
-  IStratToHarvest,
-  IChain,
-  IChains,
-} from './interfaces';
-import { setKey, getKey, redisDisconnect } from '../utility/redisHelper';
-import { logger } from '../utility/Logger';
+import type { IVault, IStratToHrvst, IChain, IChains } from './interfaces';
 import { estimateGas } from '../utils/harvestHelpers';
 import BROADCAST from '../utils/broadcast';
 
@@ -219,50 +210,53 @@ async function main(): Promise<void> {
     encountered: Set<string> = new Set();
   let dirty = false;
 
-  //Object.values( <Readonly< IChains>> require( '../data/chains.js')).forEach( (chain: IChain) =>  {
-  await Promise.all(
-    Object.values(<Readonly<IChains>>require('../data/chains.js')).map(async (chain: IChain) => {
-      const stratManager = new ChainStratManager(chain, vaults, encountered, hits);
-      if (stratManager.syncVaults(stratsToHarvest)) dirty = true;
-      const { added, removed } = stratManager.stratsChanged();
-      if (added || removed)
-        logger.info(`Strats on ${chain.id.toUpperCase()}: ${added} added, ${removed} removed`);
-      else logger.info(`No strats added or removed from ${chain.id.toUpperCase()}`);
-      //if (false)
-      if (stratManager.notOnChainHarvest.length) {
-        logger.info(
-          `  Updating gas-limit values on Cowllector-managed ${chain.id.toUpperCase()} strats...`
-        );
-        if (await stratManager.addGasLimits(stratManager.notOnChainHarvest)) dirty = true;
-        logger.info(`    Finished gas-limit updates on ${chain.id.toUpperCase()}`);
-      }
-    })
-  ); //await Promise.all( Object.values( <Readonly< IChains>>
-  //debugger;
-  for (const stratId in stratsToHarvest) {
-    if (encountered.has(stratId)) continue;
+/*Object.values( <Readonly< IChains>> require( '../data/chains.js')).forEach( (chain: IChain) =>  {*/  await Promise.all( Object.values( <Readonly< IChains>> require( 
+                                      '../data/chains.js')).map( async (chain: IChain) => {
+    const stratManager = new ChainStratManager( chain, vaults, encountered, hits);
+    if (stratManager.SyncVaults( stratsToHarvest))
+      dirty = true;
+    const {added, removed} = stratManager.stratsChanged();
+    if (added || removed)
+      console.log( `Strats on ${chain.id.toUpperCase()}: ${added } added, ${removed
+                                                                              } removed`);
+    else
+      console.log( `No strats added or removed from ${chain.id.toUpperCase()}`);
 
-    delete stratsToHarvest[stratId];
-    hits.add(stratId, 'removed, decomissioned');
-  } //for (const stratId in stratsToHarvest)
+/*if(false)*/   if (stratManager.notOnChainHarvest.length)  {
+      console.log( `  Updating gas-limit values on Cowllector-managed ${
+                                                      chain.id.toUpperCase()} strats...`);
+/**/  if (await stratManager.AddGasLimits( stratManager.notOnChainHarvest))
+/**/  dirty = true;
+      console.log( `    Finished gas-limit updates on ${chain.id.toUpperCase()}`);
+    }
+  })); //await Promise.all( Object.values( <Readonly< IChains>>
+//debugger;
+  stratsToHarvest.forEach( (strat, index) => {
+    if (encountered.has( strat.id))
+      return;
 
-  const count = Object.keys(hits.hits).length;
-  if (count) {
-    await BROADCAST.send({
-      type: 'info',
-      title: `Strat-harvest sync`,
-      message: `${
-        count < 50
-          ? `\n\`\`\`json\n${JSON.stringify(Object.values(hits.hits), null, 2)}\n\`\`\``
-          : '50+ changes: see link'
-      }`,
-    });
-    logger.info(`\nLog of ${count} significant changes written to logging channel`);
-  } else logger.info('\nNo significant changes discovered.');
+    delete stratsToHarvest[ index];
+    hits.add( strat.id, 'removed, decomissioned');
+  }); //stratsToHarvest.forEach( strat
 
-  if (dirty) setKey(REDIS_KEY, stratsToHarvest);
+  const index = Object.keys(hits.hits).length;
+  if (index) {
+    FS.writeFileSync(
+      PATH.join(__dirname, '../data/stratsSync.json'),
+      JSON.stringify(Object.values(hits.hits), null, 2)
+    );
+    console.log(`\nLog of ${index} significant changes written to data/stratsSync.json`);
+  }
 
-  await redisDisconnect();
+  if (dirty)
+    FS.writeFileSync(
+      PATH.join(__dirname, '../data/stratsToHarvest.json'),
+      JSON.stringify(
+        stratsToHarvest.filter(strat => strat),
+        null,
+        2
+      )
+    );
 } //function async main(
 
 main();
