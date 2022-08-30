673c7f7d0d917d27486f2e84e29ccdf7e21e5dd9
AllTradesz <alltrades.beefy@gmail.com>
2022-08-30 17:56:44 +0200

    Operational Gelato-lifecycle sync (Link T-622)

--- gelato/gelatoClient.ts
+++ gelato/gelatoClient.ts
@@ -1,120 +1,139 @@
-import { Contract, ethers, Wallet } from 'ethers';
+import { Contract, ethers, type Wallet, type ContractTransaction } from 'ethers';
+import { type IChain } from './interfaces';
+import { GelatoOpsSDK } from '@gelatonetwork/ops-sdk';
 import OPS_ABI from './abis/Ops.json';
 
 
 export class GelatoClient {
-  //when task is not prepaid, the contract uses the null  address, signifying to use any 
+  //when task is not prepaid, the contract uses the null address, signifying to use any 
   //  funds in the treasury
   private static readonly _feeTokenWhenNotPrepaidTask = 
-                                            "0x0000000000000000000000000000000000000000";
+                                            '0x0000000000000000000000000000000000000000';
 
-  private readonly _gelatoAdmin: Wallet;
-  private readonly _harvesterAddress: string;
   private readonly _opsContract: Contract;
-  private readonly _shouldLog: boolean;
+  private readonly _gelato: GelatoOpsSDK;
 
 
-  constructor( gelatoAdmin_: Wallet, 
-                harvesterAddress_: string, 
-                opsAddress_: string, 
-                shouldLog: boolean) {
-    this._gelatoAdmin = gelatoAdmin_;
-    this._harvesterAddress = harvesterAddress_;
-    this._opsContract = new Contract(opsAddress_, OPS_ABI, this._gelatoAdmin);
-    this._shouldLog = shouldLog;
+  constructor( private readonly _gelatoAdmin: Readonly< Wallet>, 
+                private readonly _chain: Readonly< IChain & 
+                                  {ochHarvester: NonNullable< IChain[ 'ochHarvester']>, 
+                                  ochOperations: NonNullable< IChain[ 'ochOperations']>}>, 
+                private readonly _shouldLog: boolean) {
+    this._opsContract = new Contract( _chain.ochOperations, OPS_ABI, _gelatoAdmin);
+    this._gelato = new GelatoOpsSDK( _chain.chainId, _gelatoAdmin);
   }
 
 
-  public async getGelatoAdminTaskIds() : Promise< string[]> {
-    const taskIds = await this._opsContract.getTaskIdsByUser(this._gelatoAdmin.address);
-    this._log(`Retrieved ${taskIds.length} task ids.`)
+  public async getGelatoAdminTaskIds() : Promise< ReadonlyArray< string>> {
+    const taskIds = <ReadonlyArray< string>> await this._opsContract.getTaskIdsByUser( 
+                                                                this._gelatoAdmin.address);
+    this._log( `Retrieved ${taskIds.length} task ids.`)
     return taskIds;
   }
 
 
   public async computeTaskId( vault_: string) : Promise< string> {
-    const performSelector = await this._opsContract.getSelector("performUpkeep(address,uint256,uint256,uint256,uint256,bool)");
-    const checkerSelector = (await this._opsContract.getSelector("checker(address)"));
+    const performSelector = await this._opsContract.getSelector( 
+                            "performUpkeep(address,uint256,uint256,uint256,uint256,bool)");
+    const checkerSelector = await this._opsContract.getSelector("checker(address)");
     const replaced0x = `000000000000000000000000${vault_.toLowerCase().slice(2)}`
     const resolverData = `${checkerSelector}${replaced0x}`
     const useTaskTreasuryFunds = true;
   
-    this._log(`Getting resolver hash for ${vault_}`);
+    if (this._shouldLog)  {
+      this._log(`Getting resolver hash for ${vault_}`);
+      this._log("getResolverHash data:");
+      this._log(`resolver: ${this._chain.ochHarvester}`);
+      this._log(`resolverData: ${resolverData}`);
+    }
   
-    this._log("getResolverHash data:");
-    this._log(`resolver: ${this._harvesterAddress}`);
-    this._log(`resolverData: ${resolverData}`);
+    const resolverHash = await this._opsContract.getResolverHash( this._chain.ochHarvester,  
+                                                                            resolverData);
   
-    const resolverHash = await this._opsContract.getResolverHash(
-      this._harvesterAddress,
-      resolverData
-    )
+    if (this._shouldLog)  {
+      this._log(`Getting taskId for vault: ${vault_}`);
+      this._log("getTaskId data:");
+      this._log(`taskCreator: ${this._gelatoAdmin.address}`)
+      this._log(`execAddress: ${this._chain.ochHarvester}`);
+      this._log(`selector: ${performSelector}`);
+      this._log(`useTaskTreasuryFunds: ${useTaskTreasuryFunds}`);
+      this._log(`feeToken: ${GelatoClient._feeTokenWhenNotPrepaidTask}`);
+      this._log(`resolverHash: ${resolverHash}`);
+    }
   
-    this._log(`Getting taskId for vault: ${vault_}`);
-  
-    this._log("getTaskId data:");
-    this._log(`taskCreator: ${this._gelatoAdmin.address}`)
-    this._log(`execAddress: ${this._harvesterAddress}`);
-    this._log(`selector: ${performSelector}`);
-    this._log(`useTaskTreasuryFunds: ${useTaskTreasuryFunds}`);
-    this._log(`feeToken: ${GelatoClient._feeTokenWhenNotPrepaidTask}`);
-    this._log(`resolverHash: ${resolverHash}`);
-  
-    const id = await this._opsContract.getTaskId(
-      this._gelatoAdmin.address,
-      this._harvesterAddress,
-      performSelector,
-      useTaskTreasuryFunds,
-      GelatoClient._feeTokenWhenNotPrepaidTask,
-      resolverHash
-    );
+    const id = await this._opsContract.getTaskId( this._gelatoAdmin.address,
+                                  this._chain.ochHarvester, performSelector, 
+                                  useTaskTreasuryFunds, 
+                                  GelatoClient._feeTokenWhenNotPrepaidTask, resolverHash);
   
     this._log(`Task id: ${id}`);
     return id;
-  }
+  } //public async computeTaskId(
 
-  public async createTasks(vaultMap: Record<string, string>): Promise<void> {
-    for (const key in vaultMap) {
-      const vault = vaultMap[key];
-      this._log(`Creating task for ${key}`);
+
+  public async createTasks( vaults: Readonly< Record< string, string>>) : Promise< void> {
+    for (const key in vaults) {
+      const vault: string = vaults[ key];
+      this._log( `Creating task for ${key}`);
       try {
-        await this._createTask(vault);
-        this._log('Task created');
-      } catch (e) {
-        this._log(e as string);
-        this._log(`Failed for ${key}`);
+        const taskId: string = await this._createTask( vault);
+        console.log( `Gelato task created for ${key} on ${this._chain.id.toUpperCase()
+                                                                  }: taskId = ${taskId}`);
+        await this._gelato.renameTask( taskId, key);
+      } catch (e: unknown) {
+        this._log( e as string);
+        console.log( `Failed to fully form Gelato task for ${key} on ${
+                                                          this._chain.id.toUpperCase()}`);
       }
     }
-  }
+  } //public async createTasks(
 
-  private async _createTask(vault: string) {
 
+  private async _createTask( vault: string) : Promise< string> {
     const performSelector = await this._opsContract.getSelector(
-      'performUpkeep(address,uint256,uint256,uint256,uint256,bool)'
-    );
-    const checkerSelector = await this._opsContract.getSelector('checker(address)');
+                            'performUpkeep(address,uint256,uint256,uint256,uint256,bool)');
+    const checkerSelector = await this._opsContract.getSelector( 'checker(address)');
     const replaced0x = `000000000000000000000000${vault.toLowerCase().slice(2)}`;
     const resolverData = `${checkerSelector}${replaced0x}`;
 
-    this._log('Create task data:');
-    this._log(`execAddress: ${this._harvesterAddress}`);
-    this._log(`execSelector: ${performSelector}`);
-    this._log(`resolverAddress: ${this._harvesterAddress}`);
-    this._log(`resolverData: ${resolverData}`);
+    if (this._shouldLog)  {
+      this._log( 'Create task data:');
+      this._log( `execAddress: ${this._chain.ochHarvester}`);
+      this._log( `execSelector: ${performSelector}`);
+      this._log( `resolverAddress: ${this._chain.ochHarvester}`);
+      this._log( `resolverData: ${resolverData}`);
+    }
 
-    const txn = await this._opsContract.createTask(
-      this._harvesterAddress,
-      performSelector,
-      this._harvesterAddress,
-      resolverData
-    );
-
+    const taskId: string = (await this._opsContract.callStatic.createTask(
+                                      this._chain.ochHarvester, performSelector,
+                                      this._chain.ochHarvester, resolverData)).toString();
+    const txn: ContractTransaction = await this._opsContract.createTask(
+                                                this._chain.ochHarvester, performSelector, 
+                                                this._chain.ochHarvester, resolverData);
     await txn.wait();
-  }
+    return taskId;
+  } //private async _createTask(
 
-  private _log(_log: string) {
+
+  public async deleteTasks( taskIds: ReadonlySet< string>) : Promise< void> {
+    for (const taskId of taskIds)
+      try {
+        const {tx: txn}: {tx: ContractTransaction} = await this._gelato.cancelTask( 
+                                                                                  taskId);
+        await txn.wait();
+        console.log( `Gelato task deleted on ${this._chain.id.toUpperCase()}: taskId = ${
+                                                                                taskId}`);
+      } catch (e: unknown) {
+        this._log( e as string);
+        console.log( `Failed to delete Gelato task on ${
+                                      this._chain.id.toUpperCase()}: taskId = ${ taskId}`);
+      }
+  } //public async deleteTasks( 
+
+
+  private _log( _log: string) : void {
     if (this._shouldLog) {
-      console.log(_log);
+      console.log( _log);
     }
   }
 } //class GelatoClient 
