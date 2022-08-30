eabced36c0af97f3f95a877bacf76a55c5619d55
AllTradesz <alltrades.beefy@gmail.com>
2022-08-30 17:56:44 +0200

    Operational Gelato-lifecycle sync (Link T-622)

--- gelato/index.ts
+++ gelato/index.ts
@@ -1,44 +1,32 @@
-import 'dotenv/config';
+import 'dotenv/config'
+
 import { ethers, Wallet } from 'ethers';
-import { NonceManage } from '../utility/NonceManage';
 import { TaskSyncer } from './taskSyncer';
-import { IChain, IChainHarvester, IChains } from './interfaces';
-import { logger, Logger } from '../utility/Logger';
+import { IChain, IChainOch, IChains } from './interfaces';
 
-//logger.setLevel( 'NonceManage', Logger.levels.DEBUG);
-//logger.setLevel( 'GelatoClient', Logger.levels.DEBUG);
 
-function chainIsOnChainHarvestingType(test: IChainHarvester | IChain): test is IChainHarvester {
-  return (
-    'string' === typeof test.addressHarvester &&
-    !!test.addressHarvester.length &&
-    'string' === typeof test.addressHarvesterOperations &&
-    !!test.addressHarvesterOperations.length
-  );
-}
-
-const run = async (): Promise<void> => {
+const run = async (): Promise< void> => {
   const pk = process.env.GELATO_ADMIN_PK!;
 
-  //Object.values( <Readonly< IChains>> require( '../data/chains.js')).forEach( (chain: IChain | IChainHarvester) =>  {
-  await Promise.all(
-    Object.values(<Readonly<IChains>>require('../data/chains.js')).map(
-      async (chain: IChain | IChainHarvester) => {
-        if (!chain.hasOnChainHarvesting) return;
-        if (!chainIsOnChainHarvestingType(chain)) {
-          logger.error(`On-chain harvesting misconfigured for ${chain.id.toUpperCase()}`);
-          return;
-        }
+  /*Object.values( <Readonly< IChains>> require( '../data/chains.js')).forEach( (chain: IChain) =>  {*/
+  await Promise.all( Object.values( <Readonly< IChains>> require( 
+                          '../data/chains.js')).map( async (chain: IChain | IChainOch) => {
+    if (!chain.hasOnChainHarvesting)
+      return;
+    if (!((test: IChainOch | IChain) : test is IChainOch => 'string' === 
+                    typeof test.ochHarvester && !!test.ochHarvester.length && 'string' === 
+                    typeof test.ochOperations && !!test.ochOperations.length)( chain))  {
+      console.log( `On-chain harvesting misconfigured for ${chain.id.toUpperCase()}`);
+      return;
+    }
 
-        const gelatoAdminWallet: NonceManage = new NonceManage(
-          new Wallet(pk, new ethers.providers.JsonRpcProvider(chain.rpc))
-        );
+    const provider = new ethers.providers.JsonRpcProvider( chain.rpc);
+    const gelatoAdminWallet: Wallet = new Wallet( pk, provider);
 
-        logger.info(`>>>>> on-chain harvester sync: ${chain.id.toUpperCase()}`);
-        new TaskSyncer(gelatoAdminWallet, chain).syncVaultHarvesterTasks();
-      }
-    )
-  ); //await Promise.all( Object.values( <Readonly< IChains>>
+    console.log( '>>>>> on-chain harvester sync: ', chain.id);
+    new TaskSyncer( gelatoAdminWallet, chain).syncVaultHarvesterTasks();
+  })); //await Promise.all( Object.values( <Readonly< IChains>>
 }; //const run = async (): Promise< void>
+
 
 run();
