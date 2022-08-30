b3bdca92e249e279fb3ea4e82e1fb78c961f1235
AllTradesz <alltrades.beefy@gmail.com>
2022-08-30 17:56:44 +0200

    Operational Gelato-lifecycle sync (Link T-622)

--- gelato/interfaces.ts
+++ gelato/interfaces.ts
@@ -1,8 +1,8 @@
-import { IStratToHarvest, IChain as _IChain, IChains } from '../scripts/interfaces';
-export { IStratToHarvest, IChains };
+import { IStratToHrvst, IChain as _IChain, IChains } from '../scripts/interfaces';
+export { IStratToHrvst, IChains };
 
 /*
-export interface IStratToHarvest {
+export interface IStratToHrvst {
   id: string;
   chain: string;
   earnContractAddress: string; //i.e. the vault's contract address
@@ -11,14 +11,14 @@ export interface IStratToHarvest {
   lastHarvest: number;
   noOnChainHrvst?: boolean;
   interval?: number;
-} //interface IStratToHarvest 
+} //interface IStratToHrvst 
 */
 export interface IChain extends _IChain {
-  readonly addressHarvester?: string;
-  readonly addressHarvesterOperations?: string;
+  readonly ochHarvester?: string;
+  readonly ochOperations?: string;
 }
 
-export interface IChainHarvester extends IChain {
-  readonly addressHarvester: string;
-  readonly addressHarvesterOperations: string;
+export interface IChainOch extends IChain {
+  readonly ochHarvester: string;
+  readonly ochOperations: string;
 }
