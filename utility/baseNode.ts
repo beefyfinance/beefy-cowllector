8042cf70d61fa4bac405d057f0486d829e673b28
AllTradesz <alltrades.beefy@gmail.com>
2022-09-05 16:53:43 +0200

    NonceManager metering PoC with debug logging in case ever needed

--- utility/baseNode.ts
+++ utility/baseNode.ts
@@ -1,12 +1,7 @@
-export const settledPromiseRejected = (result: PromiseSettledResult< unknown>) :
-																				result is PromiseRejectedResult => 
+export const settledPromiseRejected = (result: PromiseSettledResult< unknown>) : 
+																result is PromiseRejectedResult => 
 																									'rejected' === result.status;
 export const settledPromiseFilled = <T> (result: PromiseSettledResult< T>) : 
-																				result is PromiseFulfilledResult< T> => 
+																	result is PromiseFulfilledResult< T> => 
 																									'fulfilled' === result.status;
 
-export const swapKeyValues = <T extends Record< string, string | number>> 
-																						(obj: T, numeric?: boolean) : 
-																						{ [K in keyof T as T[ "K"]]: K} => 
-											Object.fromEntries( Object.entries( <any> obj).map( 
-											([key, value]) => [value, numeric ? parseInt( key) : key]));
