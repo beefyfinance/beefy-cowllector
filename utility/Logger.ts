432f7276cb65b69c170c7531c11bd106a305a5d1
AllTradesz <alltrades.beefy@gmail.com>
2022-11-07 21:39:19 +0100

    preserve rewriteFrames test code

--- utility/Logger.ts
+++ utility/Logger.ts
@@ -9,11 +9,13 @@ Usage:
 	instance by the import: 
 import { logger } from '../yourpath/logger'
 + In the application entry-point module, also import the main `Logger` class so 
-you may adjust the default configuration, like setting a default logging level, 
-e.g. static call of 
-Logger.setDefaultLevel( Logger.levels.WARN) 
+you may adjust the default configuration, like (1) setting a default logging 
+level, e.g. 
+Logger.instance.setDefaultLevel( Logger.levels.WARN) 
 to decrease verbosity from the default of  
 Logger.levels.INFO
+or (2) setting up a central Sentry integration
+Logger.instance.initializeSentry( {dsn: <your DSN URL>, tracesSampleRate: 1.0})
 + Wherever a specialized, separately configurable logger is desired, it can be 
 obtained via a call against the main logger instance: 
 logger.getLogger( 'YourName')
@@ -28,162 +30,196 @@ This way, your main module may centrally manage the logging over the system.
 **/
 import loglevel from 'loglevel';
 import * as Sentry from '@sentry/node';
+//import {RewriteFrames} from '@sentry/integrations';
+import type {NodeOptions} from '@sentry/node/types/types';
+//import * as Tracing from '@sentry/tracing';
 import LoglevelSentryPlugin from '@toruslabs/loglevel-sentry';
-import { swapKeyValues } from '../utility/baseNode';
+import {swapKeyValues} from '../utility/baseNode';
 
-type NamedRootLog = loglevel.RootLogger & { name: string | undefined };
-type NamedLog = loglevel.Logger & { name: string };
+type NamedRootLog = loglevel.RootLogger & {name: string | undefined};
+type NamedLog = loglevel.Logger & {name: string};
 
-export class Logger implements ILogger {
+
+export class Logger implements ILogger  {
   public static readonly levels: loglevel.LogLevel = loglevel.levels;
-  public static readonly levelNames: Readonly<Record<string, string>> = swapKeyValues(
-    <Partial<loglevel.LogLevel>>Logger.levels
-  );
+  public static readonly levelNames: Readonly< Record< string, string>> = 
+                                            swapKeyValues( <Partial< 
+                                            loglevel.LogLevel>> Logger.levels);
 
   private static readonly _singleton: Logger = new Logger();
-  private static readonly _mainLog = <NamedRootLog>(<unknown>loglevel);
+  private static readonly _mainLog = <NamedRootLog> <unknown> loglevel;
 
-  public static get instance(): Logger {
+  public static get instance() : Logger {
     return this._singleton;
   }
 
-  private static subLoggerSet(
-    test: InstanceType<typeof Logger.SubLogger> | loglevel.LogLevelDesc
-  ): test is InstanceType<typeof Logger.SubLogger> {
+  private static subLoggerSet( test: InstanceType< typeof Logger.SubLogger> | 
+                                                      loglevel.LogLevelDesc) : 
+                              test is InstanceType< typeof Logger.SubLogger>  {
     return test instanceof Object;
   }
 
-  private readonly _sentry = new LoglevelSentryPlugin(Sentry);
-  private _sentryLogger: NamedLog | undefined;
-  private _children = {} as Record<
-    string,
-    InstanceType<typeof Logger.SubLogger> | loglevel.LogLevelDesc
-  >;
+// private readonly _sentry = new LoglevelSentryPlugin( Sentry);
+  private _sentryLogger: NamedLog | boolean = false;
+  private _children  = {} as Record< string, InstanceType< 
+                              typeof Logger.SubLogger> | loglevel.LogLevelDesc>;
 
   private constructor() {}
 
-  public setLevel(name: string, level: loglevel.LogLevelDesc): boolean {
-    let entry: InstanceType<typeof Logger.SubLogger> | loglevel.LogLevelDesc;
-    if ('main' === name.toLowerCase()) Logger._mainLog.setLevel(level, false);
-    else if (Logger.subLoggerSet((entry = this._children[name]))) entry.setLevel(level);
-    else {
-      this._children[name] = level;
+
+  public setLevel( name: string, 
+                    level: loglevel.LogLevelDesc) : boolean {
+    let entry: InstanceType< typeof Logger.SubLogger> | loglevel.LogLevelDesc;
+    if ('main' === name.toLowerCase())
+      Logger._mainLog.setLevel( level, false);
+    else if (Logger.subLoggerSet( entry = this._children[ name]))
+      entry.setLevel( level);
+    else  {
+      this._children[ name] = level;
       return false;
     }
     return true;
   } //setLevel(
 
-  public setDefaultLevel(level: loglevel.LogLevelDesc): void {
-    Logger._mainLog.setDefaultLevel(level);
+
+  public setDefaultLevel( level: loglevel.LogLevelDesc) : void  {
+    Logger._mainLog.setDefaultLevel( level);
   }
+
+	public initializeSentry( initializer: Readonly< NodeOptions>) :  void	{
+//	const rewriteFrames: typeof initializer.integrations = [new RewriteFrames( 
+//																									{root: global.__dirname})];
+		Sentry.init( initializer);//{integrations: initializer.integrations ? 
+														/*rewriteFrames.concat( initializer.integrations)*/ 
+/*														 <typeof initializer.integrations> 
+														 [rewriteFrames[0], ...<any> 
+														initializer.integrations] : rewriteFrames, 
+														...initializer});
+*/	this._sentryLogger = true;
+	}
 
   public get name() {
     return Logger._mainLog.name;
   }
 
-  public info(msg: string): void {
-    Logger._mainLog.info(msg);
-    this.sentry(this, loglevel.levels.INFO, msg);
+  public info( msg: string) : void  {
+    Logger._mainLog.info( msg);
+    this.sentry( this, loglevel.levels.INFO, msg);
   }
 
-  public warn(msg: string): void {
-    Logger._mainLog.warn(msg);
-    this.sentry(this, loglevel.levels.WARN, msg);
+  public warn( msg: string) : void  {
+    Logger._mainLog.warn( msg);
+    this.sentry( this, loglevel.levels.WARN, msg);
   }
 
-  public error(msg: string): void {
-    Logger._mainLog.error(msg);
-    this.sentry(this, loglevel.levels.ERROR, msg);
+  public error( msg: string) : void {
+    Logger._mainLog.error( msg);
+    this.sentry( this, loglevel.levels.ERROR, msg);
   }
 
-  public debug(msg: string): void {
-    Logger._mainLog.debug(msg);
-    this.sentry(this, loglevel.levels.DEBUG, msg);
+  public debug( msg: string) : void {
+    Logger._mainLog.debug( msg);
+    this.sentry( this, loglevel.levels.DEBUG, msg);
   }
 
-  public trace(msg: string): void {
-    Logger._mainLog.trace(msg);
-    this.sentry(this, loglevel.levels.TRACE, msg);
+  public trace( msg: string) : void {
+    Logger._mainLog.trace( msg);
+    this.sentry( this, loglevel.levels.TRACE, msg);
   }
 
-  public get sentryLogger(): ILogger {
-    if (!this._sentryLogger) {
-      this._sentry.install((this._sentryLogger = <NamedLog>loglevel.getLogger('Beefy Sentry')));
-      this._sentryLogger.setLevel(loglevel.levels.ERROR);
+  public get sentryLogger() : ILogger | null {
+		if (!this._sentryLogger)
+			return null;
+    if ('boolean' == typeof this._sentryLogger)  {
+      (new LoglevelSentryPlugin( Sentry)).install( this._sentryLogger = 
+																<NamedLog> loglevel.getLogger( 'Beefy Sentry'));
+      this._sentryLogger.setLevel( loglevel.levels.ERROR);
     }
     return this._sentryLogger;
   }
 
-  private sentry(_log: Readonly<ILogger>, level: loglevel.LogLevelNumbers, msg: string) {
-    if (false)
-      if (loglevel.levels.ERROR <= level) {
-        //TODO: figure out the TS to get rid of the ugly "any" cast
-        (<any>this.sentryLogger)[Logger.levelNames[level].toLowerCase()](
-          `${_log.name}, ${Logger.levelNames[level]}: ${msg}`
-        );
-        return;
-      }
 
-    //if the situation matches a rule set for Sentry output, send the message
+  private sentry( _log: Readonly< ILogger>, 
+                  level: loglevel.LogLevelNumbers, 
+                  msg: string) : void  {
+		if (!this.sentryLogger)
+			return;
+
+    if (loglevel.levels.ERROR <= level) {
+//TODO: figure out the TS to get rid of the ugly "any" cast
+      (<any> this.sentryLogger)[ Logger.levelNames[ level].toLowerCase()]( 
+																				`${this.name ? this.name + ', ' : ''}${
+																				Logger.levelNames[ level]}: ${msg}`);
+      return;
+    }
+
+    //if the situation matches a rule set for Sentry output, send the message 
     //  to Sentry
   } //private sentry(
+                  
 
-  public getLogger(name: string): ILogger {
-    let entry: InstanceType<typeof Logger.SubLogger> | loglevel.LogLevelDesc | undefined =
-      this._children[name];
-    if (!Logger.subLoggerSet(entry)) {
-      const subLogger = (this._children[name] = new Logger.SubLogger(name, this));
-      if ('undefined' !== typeof entry) subLogger.setLevel(entry);
+  public getLogger( name: string) : ILogger {
+    let entry: InstanceType< typeof Logger.SubLogger> | 
+                loglevel.LogLevelDesc | undefined = this._children[ name];
+    if (!(Logger.subLoggerSet( entry))) {
+      const subLogger = this._children[ name] = new Logger.SubLogger( name, 
+                                                                         this);
+      if ('undefined' !== typeof entry)
+        subLogger.setLevel( entry);
       entry = subLogger;
     }
     return entry;
   } //public getLogger(
 
+
   protected static SubLogger = class implements ILogger {
     private _logger: NamedLog;
-    constructor(readonly name: string, private parent: Logger) {
-      this._logger = <NamedLog>Logger._mainLog.getLogger(name);
+    constructor( readonly name: string, 
+                  private parent: Logger) {
+      this._logger = <NamedLog> Logger._mainLog.getLogger( name);
     }
 
-    public setLevel(level: loglevel.LogLevelDesc, persist?: boolean): void {
-      this._logger.setLevel(level, persist);
+    public setLevel( level: loglevel.LogLevelDesc, 
+                      persist?: boolean) : void {
+      this._logger.setLevel( level, persist);
     }
 
-    public info(msg: string): void {
-      this._logger.info(`${this.name}: ${msg}`);
-      this.parent.sentry(this, loglevel.levels.INFO, msg);
+    public info( msg: string) : void  {
+      this._logger.info( `${this.name}: ${msg}`);
+      this.parent.sentry( this, loglevel.levels.INFO, msg);
     }
 
-    public warn(msg: string): void {
-      this._logger.warn(`${this.name}: ${msg}`);
-      this.parent.sentry(this, loglevel.levels.WARN, msg);
+    public warn( msg: string) : void  {
+      this._logger.warn( `${this.name}: ${msg}`);
+      this.parent.sentry( this, loglevel.levels.WARN, msg);
     }
 
-    public error(msg: string): void {
-      this._logger.error(`${this.name}: ${msg}`);
-      this.parent.sentry(this, loglevel.levels.ERROR, msg);
+    public error( msg: string) : void {
+      this._logger.error( `${this.name}: ${msg}`);
+      this.parent.sentry( this, loglevel.levels.ERROR, msg);
     }
 
-    public debug(msg: string): void {
-      this._logger.debug(`${this.name}: ${msg}`);
-      this.parent.sentry(this, loglevel.levels.DEBUG, msg);
+    public debug( msg: string) : void {
+      this._logger.debug( `${this.name}: ${msg}`);
+      this.parent.sentry( this, loglevel.levels.DEBUG, msg);
     }
 
-    public trace(msg: string): void {
-      this._logger.trace(`${this.name}: ${msg}`);
-      this.parent.sentry(this, loglevel.levels.TRACE, msg);
+    public trace( msg: string) : void {
+      this._logger.trace( `${this.name}: ${msg}`);
+      this.parent.sentry( this, loglevel.levels.TRACE, msg);
     }
-  }; //static SubLogger = class implements ILogger
+  } //static SubLogger = class implements ILogger
 } //class Logger
 
-Logger.instance.setDefaultLevel(Logger.levels.INFO);
+Logger.instance.setDefaultLevel( Logger.levels.INFO);
 export const logger: Logger = Logger.instance;
+
 
 export interface ILogger {
   readonly name: string | undefined;
-  info(msg: string): void;
-  warn(msg: string): void;
-  error(msg: string): void;
-  debug(msg: string): void; //TODO: extend these to handle concatenated ...args
-  trace(msg: string): void;
+  info( msg: string) : void; //TODO: extend these to handle concatenated ...args
+  warn( msg: string) : void;
+  error( msg: string) : void;
+  debug( msg: string) : void;
+  trace( msg: string) : void;
 }
