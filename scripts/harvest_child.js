4e694dc248e12e301ec8756e17753077122e3516
AllTradesz <alltrades.beefy@gmail.com>
2023-01-01 22:02:01 +0100

    ultra-tiny fix

--- scripts/harvest_child.js
+++ scripts/harvest_child.js
@@ -379,7 +379,7 @@ const harvest = async (strat,
 							}:\tharvested step 3/3: Swaps to tokens needed step - with tx: ${
 							tx.transactionHash}`);
               return {contract: strat.strategy || strat.address,
-											status: ' success',
+											status: 'success',
 											message: `${strat.id || strat.name
 																}: harvested after try ${tries} - with tx: ${
 																tx.transactionHash}`,
